358670ca1afff18ccfcaf7e09454537c4c2c362a
HANSEO PARK <hapark@cern.ch>
2024-01-23 00:38:21 +0900

    Add scirpts and small fix

---
+++ startAnalysisTCHfJetTagging.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script processes root files for different jet types and categories using PlotHfJetTaggingTask.C
+# It expects two arguments:
+#   1. Input root file for data (e.g., AnalysisResult.root)
+#   2. Input root file for simulation
+
+# Example usage for creating run.sh:
+#   #!/bin/bash
+#   TCDATAINPUT=/path/to/data
+#   TCSIMINPUT=/path/to/simulation
+#   bash startAnalysisHfJetTagging.sh $TCDATAINPUT $TCSIMINPUT
+
+# Define the jet types and data categories
+jet_types=("incjet" "lfjet" "cjet" "bjet")
+data_types=("data" "sim")
+
+# Loop through jet types and data categories to create directories
+for jet_type in "${jet_types[@]}"; do
+  for data_type in "${data_types[@]}"; do
+    basedir="fig/${data_type}/${jet_type}"
+
+    # Check if the directory already exists
+    if [ ! -d "$basedir" ]; then
+      # The directory does not exist, so create it
+      mkdir -p "$basedir"
+      echo "Created directory: $basedir"
+    else
+      # The directory already exists, skip creating it
+      echo "Directory already exists, skipping: $basedir"
+    fi
+  done
+done
+
+# Execute the root command with the provided input files
+# $1: Input root file for data, $2: Input root file for simulation
+root -x -l -b -q "task/PlotHfJetTCTaggingTask.C(\"$1\", \"$2\")"
---
+++ task/PlotHfJetTCTaggingTask.C
@@ -0,0 +1,28 @@
+#include "../include/DataPoint.h"
+#include "../include/HfJetTaggingTCAnalysis.h"
+#include <TString.h>
+
+void PlotHfJetTaggingTCQA(TString rootdata, TString rootsim) {
+  bool doData = false;
+  bool doMC = true;
+  bool dopartLevel = false;
+  bool withInc = false;
+  bool doLog = true;
+
+  HfJetTaggingTCAnalysis *HfJetTagTCObj = new HfJetTaggingTCAnalysis(rootdata.Data(), rootsim.Data(), doData, doMC, dopartLevel);
+  HfJetTagTCObj->DrawTaggedJetSignImpXYSignificanceN1(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpXYSignificanceN2(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpXYSignificanceN3(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpZSignificanceN1(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpZSignificanceN2(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpZSignificanceN3(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpXYZSignificanceN1(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpXYZSignificanceN2(withInc, doLog);
+  HfJetTagTCObj->DrawTaggedJetSignImpXYZSignificanceN3(withInc, doLog);
+
+}
+
+void PlotHfJetTCTaggingTask(TString rootdata="", TString rootsim="") {
+  PlotHfJetTaggingTCQA(rootdata.Data(), rootsim.Data());
+}
+
