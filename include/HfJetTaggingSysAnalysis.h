31f619487c167ae2f16ae371a2e42a57f5fba3fb
HANSEO PARK <hapark@cern.ch>
2024-02-15 00:00:29 +0900

    change tempale document

--- include/HfJetTaggingSysAnalysis.h
+++ include/HfJetTaggingSysAnalysis.h
@@ -3,30 +3,75 @@
 
 #include <TH1F.h>
 #include <TString.h>
+#include "CanvasHandler.h"
 #include "DataPoint.h"
 
 class HfJetTaggingSysAnalysis {
   public:
-    HfJetTaggingSysAnalysis(bool doData, bool doMC, bool dopartLevel);
+    HfJetTaggingSysAnalysis(const std::vector<TString> &rootSim, bool doData, bool doMC, bool dopartLevel) {
+///      if (doData) {
+///        LoadData(rootSim);
+///      }
+      if (doMC) {
+        if(!LoadSim(rootSim)) {
+          return;
+        }
+        InitHistogram();
+        ProjectionHist();
+        NormalizedHistogram();
+      }
+
+    }
     ~HfJetTaggingSysAnalysis();
+
+    //// FUNCTION ////
+    int LoadSim(const std::vector<TString> &rootFiles);
+    void InitHistogram();
+    void ProjectionHist();
+    void NormalizedHistogram();
 
     // fucntion
     void HistColorStyle(TH1F* h1, int mc, int ms, double mS, int lc, int ls);
-    int canvasNum=0;
+    void DrawCombined(int num, const std::vector<HistogramData>& histList, bool doLeg, double legendxmin, double ymin, double xmax, double ymax);
+
   protected:
+    CanvasHandler* canvasHandler;
+    int canvasNum=0;
 
   private:
-    static const int temp =-1;
-
 
 };
 
-HfJetTaggingSysAnalysis::HfJetTaggingSysAnalysis(bool doData, bool doMC, bool dopartLevel) {
+HfJetTaggingSysAnalysis::~HfJetTaggingSysAnalysis() {
   std::cout<< " " << std::endl;
 }
 
-HfJetTaggingSysAnalysis::~HfJetTaggingSysAnalysis() {
-  std::cout<< " " << std::endl;
+//int HfJetTaggingSysAnalysis::LoadData(TString rootFile) { // TODO
+//  return 0;
+//}
+
+int HfJetTaggingSysAnalysis::LoadSim(const std::vector<TString> &rootFiles) {
+  const TString &taskName = "jet-taggerhf-qa-charged";
+  for (const auto& rootFile : rootFiles) {
+    if (gSystem->AccessPathName(rootFile.Data())) {
+      std::cout << "Input file (MC) not found!" << std::endl;
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+void HfJetTaggingSysAnalysis::InitHistogram() {
+
+}
+
+void HfJetTaggingSysAnalysis::ProjectionHist() {
+
+}
+
+void HfJetTaggingSysAnalysis::NormalizedHistogram() {
+
 }
 
 void HfJetTaggingSysAnalysis::HistColorStyle(TH1F* h1, int markercolor = 1, int markerstyle = 20,
@@ -38,6 +83,18 @@ void HfJetTaggingSysAnalysis::HistColorStyle(TH1F* h1, int markercolor = 1, int 
   h1->SetMarkerSize(markersize);
   h1->SetLineStyle(linestyle);
   h1->SetLineColor(linecolor);
+}
+
+void HfJetTaggingSysAnalysis::DrawCombined(int num, const std::vector<HistogramData>& histograms, bool doLeg, double xmin, double ymin, double xmax, double ymax) {
+  TLegend *leg = new TLegend(xmin, ymin, xmax, ymax);
+  for (int i=0; i<num; i++){
+    TH1F *hist = histograms[i].hist;
+    this->HistColorStyle(hist, i+1, i+20, HfJetTagging::MARKERSIZE, i+1, 1);
+    TString clfi = histograms[i].classification;
+    leg->AddEntry(hist, clfi.Data(), "lep");
+    hist->Draw("same");
+  }
+  if(doLog) leg->Draw();
 }
 
 #endif // HFJETTAGGINGSYSANALYSIS_H
