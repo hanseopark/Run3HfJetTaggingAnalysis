20344ef3f42301828d2ac4948df2c3935f7cacc5
HANSEO PARK <hapark@cern.ch>
2025-10-10 15:03:43 +0900

    modification of scripts

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -48,6 +48,14 @@ while [ "$1" != "" ]; do
       shift
       ROOTSUFFIX=$1
       ;;
+    --datasuffix )
+      shift
+      DATASUFFIX=$1
+      ;;
+    --mcsuffix )
+      shift
+      SIMSUFFIX=$1
+      ;;
     -h | --help )
       usage
       exit
@@ -73,6 +81,15 @@ while [ "$1" != "" ]; do
       ;;
     --quarkhadron )
       doQuarkHadronLevel=true
+      ;;
+    --tracktuner )
+      doTrackTuner=true
+      ;;
+    --track2coll )
+      doTrack2Coll=true
+      ;;
+    --mergeSV )
+      doMergeSV=true
       ;;
     * )
       echo "Unknown option: $1"
@@ -116,6 +133,14 @@ elif [ "$DATAPASS" == "LHC22o_apass6" ]; then
   DATA_NAME="LHC22o_apass6"
 elif [ "$DATAPASS" == "apass7" ]; then
   DATA_NAME="LHC22o_apass7"
+elif [ "$DATAPASS" == "22pass7" ]; then
+  DATA_NAME="LHC22o_apass7"
+elif [ "$DATAPASS" == "23pass4" ]; then
+  DATA_NAME="LHC23_pass4_Thin"
+elif [ "$DATAPASS" == "24pass1" ]; then
+  DATA_NAME="LHC24_pass1_MinBais"
+elif [ "$DATAPASS" == "merge" ]; then
+  DATA_NAME="merge"
 else
   DATA_NAME="LHC22o_apass6"
 fi
@@ -124,33 +149,33 @@ DATAPATH="$DATAPATH/$DATA_NAME"
 if [ "$MCTRIGGER" == "jjtest" ]; then # JetJet trigger test sample
   MC_NAME="LHC23d4"
   TRIGGER_NAME="PYTHIA 8 + Geant4"
-elif [ "$MCTRIGGER" == "apass4" ]; then # General Purpose MC anchored to LHC22f apass4
-  MC_NAME="LHC23d1k"
-  TRIGGER_NAME="anchored to apass4 22f"
-elif [ "$MCTRIGGER" == "apass4highIR" ]; then # General Purpose anchored to apass4 of 13.6 TeV pp data period LHC22m/o/p/r/t, high IR
-  MC_NAME="LHC23k2f"
-  TRIGGER_NAME="anchored to apass4 LHC22m/o/p/r/t, high IR"
-elif [ "$MCTRIGGER" == "apass6" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o MinBias
-  MC_NAME="LHC24b1b"
-  TRIGGER_NAME="anchored to apass6 LHC22o"
-elif [ "$MCTRIGGER" == "apass7" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o MinBias
-  MC_NAME="LHC24f3"
-  TRIGGER_NAME="anchored to apass7 LHC22o"
-elif [ "$MCTRIGGER" == "apass6Hf" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o MinBias
+elif [ "$MCTRIGGER" == "hfapass7" ]; then # General Purpose anchored to apass7 of 13.6 TeV pp data period LHC22o MinBias
   MC_NAME="LHC24d3"
-  TRIGGER_NAME="b & c enhanced anchored to apass6 LHC22o"
-elif [ "$MCTRIGGER" == "jjapass7" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o jet-jet
+  TRIGGER_NAME="anchored to apass7 LHC22o"
+elif [ "$MCTRIGGER" == "hfapass7skim" ]; then # General Purpose anchored to apass7 of 13.6 TeV pp data period LHC22o MinBias
+  MC_NAME="LHC24f3"
+  TRIGGER_NAME="anchored to apass7 LHC22o skimmed"
+elif [ "$MCTRIGGER" == "jjapass7old" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o jet-jet
   MC_NAME="LHC24g4"
+  TRIGGER_NAME="jet-jet anchored to apass7 LHC22o"
+elif [ "$MCTRIGGER" == "jjapass7" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o jet-jet
+  MC_NAME="LHC25a2"
+  TRIGGER_NAME="jet-jet anchored to apass7 LHC22o"
+elif [ "$MCTRIGGER" == "jjapass7new" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o jet-jet
+  MC_NAME="LHC25a2b"
   TRIGGER_NAME="jet-jet anchored to apass7 LHC22o"
 elif [ "$MCTRIGGER" == "testJetAnchoredGapGen-20240723-100905" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o MinBias
   MC_NAME="testJetAnchoredGapGen-20240723-100905"
   TRIGGER_NAME="anchored to apass7 jet-jet no Gap"
-elif [ "$MCTRIGGER" == "mb" || "$MCTRIGGER" == "MB" ]; then
-  MC_NAME="LHC24b1b"
-  TRIGGER_NAME="MB"
+elif [ "$MCTRIGGER" == "mb" || "$MCTRIGGER" == "MB" ]; then #
+  MC_NAME="LHC24f3b"
+  TRIGGER_NAME="anchored to apass7 LHC22o high IR"
+#elif [ "$MCTRIGGER" == "mbli" || "$MCTRIGGER" == "MBLI" ]; then #
+#  MC_NAME="LHC24f3c"
+#  TRIGGER_NAME="anchored to apass7 LHC22o"
 else
-  MC_NAME="LHC24b1b"
-  TRIGGER_NAME="MB"
+  MC_NAME="LHC24f3b"
+  TRIGGER_NAME="jet-jet anchored to apass7 LHC22o"
 fi
 SIMPATH="$SIMPATH/$MC_NAME"
 
@@ -180,40 +205,58 @@ if [ -n "$ROOTSUFFIX" ]; then
   ROOT_DATA_NAME="AnalysisResults_$ROOTSUFFIX.root"
   ROOT_SIM_NAME="AnalysisResults_$ROOTSUFFIX.root"
 fi
+if [ -n "$DATASUFFIX" ]; then
+  ROOT_DATA_NAME="AnalysisResults_$DATASUFFIX.root"
+fi
+if [ -n "$SIMSUFFIX" ]; then
+  ROOT_SIM_NAME="AnalysisResults_$SIMSUFFIX.root"
+fi
+
+#FIGURE_NAME="ALICE Simulation"
+#FIGURE_NAME="ALICE Performance"
+FIGURE_NAME="ALICE Work in Progress"
+#FIGURE_NAME="ALICE Prepliminary"
+TRIGGER_NAME="PYTHIA 8 + Geant 4"
 
 #### CONFIGURATION ####
 PROCESS_MCP=false
 WITH_INCLUSIVE=true
 USE_LOG_SCALE=true
-FILL_RUN2DEF=false
+FILL_MATCHED=true
 FILL_TEST=false
 
 ## General jet
+WITH_MODEL=true
+FILL_SYS=true
+LOAD_EXT_SYS=false
 
 ## IP
 FILL_IP_XY=true
 FILL_IP_Z=false
 FILL_IP_XYZ=false
-DO_FIT_JP=true
+DO_FIT_JP=false
 FILL_JETRECO_EFFIANDPURITY=false
-DO_UNFOLDING=false
+VALIDATION=false
+DO_UNFOLDING=true
 
 ## TC
 FILL_TC_XY=true
-FILL_TC_Z=true
-FILL_TC_XYZ=true
-FILL_TC_EFFIANDPURITY=true
+FILL_TC_Z=false
+FILL_TC_XYZ=false
+FILL_TC_EFFIANDPURITY=false
 
 ## JP
 DO_TRACK_PROB=false
 DO_TEMPLATE_FIT=false
-FILL_JP_EFFIANDPURITY=true
+FILL_JP_EFFIANDPURITY=false
 
 ## SV
 FILL_SV_2PRONG=false
 FILL_SV_3PRONG=true
 FILL_SV_N1=true
+LOAD_RESMAT=true
 FILL_SV_EFFIANDPURITY=true
+FILL_SV_EFFIANDPURITY_IP=false
 
 ## For track efficiency
 PROCESS_TRACK_EFFI=false
@@ -227,20 +270,25 @@ SYS_OPTIONS=""
 [ "$doPass" = true ] && SYS_OPTIONS="$SYS_OPTIONS --pass"
 [ "$doTagger" = true ] && SYS_OPTIONS="$SYS_OPTIONS --tagger"
 [ "$doQuarkHadronLevel" = true ] && SYS_OPTIONS="$SYS_OPTIONS --quarkhadron"
+[ "$doTrackTuner" = true ] && SYS_OPTIONS="$SYS_OPTIONS --tracktuner"
+[ "$doTrack2Coll" = true ] && SYS_OPTIONS="$SYS_OPTIONS --track2coll"
+[ "$doMergeSV" = true ] && SYS_OPTIONS="$SYS_OPTIONS --mergeSV"
 
 DATAINPUT="$DATAPATH/$ROOT_DATA_NAME"
 SIMINPUT="$SIMPATH/$ROOT_SIM_NAME"
 if [ "$MODE" == "tag" ]; then
   #bash startAnalysisHfJetTagging.sh "$DATAINPUT" "$SIMINPUT" $PROCESS_IP_DATA $PROCESS_IP_MCD $DO_FIT_JP $DO_TC $PROCESS_JP_DATA $PROCESS_JP_MCD $DO_TEMPLATE_FIT $PROCESS_SV_2PRONG_DATA $PROCESS_SV_3PRONG_DATA $PROCESS_SV_2PRONG_MCD $PROCESS_SV_3PRONG_MCD "$TRIGGER_NAME" $IS_PARTICLE_LEVEL $WITH_INCLUSIVE $USE_LOG_SCALE "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME"
-  bash startIP.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_RUN2DEF $FILL_IP_XY $FILL_IP_Z $FILL_IP_XYZ $DO_FIT_JP $FILL_JETRECO_EFFIANDPURITY $DO_UNFOLDING
+  bash startIP.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$FIGURE_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_MATCHED $FILL_TEST $FILL_IP_XY $FILL_IP_Z $FILL_IP_XYZ $DO_FIT_JP $FILL_JETRECO_EFFIANDPURITY $VALIDATION $DO_UNFOLDING
 elif [ "$MODE" == "ip" ]; then
-  bash startIP.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_RUN2DEF $FILL_IP_XY $FILL_IP_Z $FILL_IP_XYZ $DO_FIT_JP $FILL_JETRECO_EFFIANDPURITY $DO_UNFOLDING
+  bash startIP.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$FIGURE_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_MATCHED $FILL_TEST $FILL_IP_XY $FILL_IP_Z $FILL_IP_XYZ $DO_FIT_JP $FILL_JETRECO_EFFIANDPURITY $VALIDATION $DO_UNFOLDING
+elif [ "$MODE" == "resmat" ]; then
+  bash startResMat.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$FIGURE_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_MATCHED $FILL_TEST
 elif [ "$MODE" == "tc" ]; then
-  bash startTC.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_RUN2DEF $FILL_TC_XY $FILL_TC_Z $FILL_TC_XYZ $FILL_TC_EFFIANDPURITY
+  bash startTC.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$FIGURE_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_TC_XY $FILL_TC_Z $FILL_TC_XYZ $FILL_TC_EFFIANDPURITY
 elif [ "$MODE" == "jp" ]; then
-  bash startJP.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_RUN2DEF $DO_TRACK_PROB $DO_TEMPLATE_FIT $FILL_JP_EFFIANDPURITY
+  bash startJP.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$FIGURE_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $DO_TRACK_PROB $DO_TEMPLATE_FIT $FILL_JP_EFFIANDPURITY
 elif [ "$MODE" == "sv" ]; then
-  bash startSV.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_RUN2DEF $FILL_TEST $FILL_SV_2PRONG $FILL_SV_3PRONG $FILL_SV_N1 $FILL_SV_EFFIANDPURITY
+  bash startSV.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" "$FIGURE_NAME" "$SUFFIX" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP $FILL_TEST $FILL_SV_2PRONG $FILL_SV_3PRONG $FILL_SV_N1 $LOAD_RESMAT $FILL_SV_EFFIANDPURITY $FILL_SV_EFFIANDPURITY_IP $WITH_MODEL $FILL_SYS $LOAD_EXT_SYS
 elif [ "$MODE" == "effi" ]; then
   bash startTrackEffi.sh "$DATAINPUT" "$SIMINPUT" "$SOURCE_NAME" "$DATA_NAME" "$MC_NAME" "$TRIGGER_NAME" $WITH_INCLUSIVE $USE_LOG_SCALE $PROCESS_DATA $PROCESS_MCD $PROCESS_MCP
 elif [ "$MODE" == "sys" ]; then
--- startSys.sh
+++ startSys.sh
@@ -13,7 +13,9 @@ doTrigger=false
 doPass=false
 doTagger=false
 doQuarkHadronLevel=false
-source=""
+doTrackTuner=false
+doTrack2Coll=false
+ssource=""
 data=""
 sim=""
 TRIGGER_NAME=""
@@ -42,9 +44,18 @@ while [ "$1" != "" ]; do
     --quarkhadron )
       doQuarkHadronLevel=true
       ;;
+    --tracktuner )
+      doTrackTuner=true
+      ;;
+    --track2coll )
+      doTrack2Coll=true
+      ;;
+    --mergeSV )
+      doMergeSV=true
+      ;;
     --source )
       shift
-      source=$1
+      ssource=$1
       ;;
     --data )
       shift
@@ -70,17 +81,11 @@ while [ "$1" != "" ]; do
   shift
 done
 
-if [ "$sim" == "LHC23d4" ]; then # JetJet trigger test sample
-  TRIGGER_NAME="PYTHIA 8 + Geant4 jet-jet test sample"
-elif [ "$sim" == "LHC23d1k" ]; then # General Purpose MC anchored to LHC22f apass4
-  TRIGGER_NAME="anchored to apass4 22f"
-elif [ "$sim" == "LHC23k2f" ]; then # General Purpose anchored to apass4 of 13.6 TeV pp data period LHC22m/o/p/r/t, high IR
-  TRIGGER_NAME="anchored to apass4 LHC22m/o/p/r/t, high IR"
-elif [ "$sim" == "LHC24b1b" ]; then # General Purpose anchored to apass6 of 13.6 TeV pp data period LHC22o MinBias
-  TRIGGER_NAME="anchored to apass6 LHC22o"
-else
-  TRIGGER_NAME="MB"
-fi
+#FIGURE_NAME="ALICE Simulation"
+#FIGURE_NAME="ALICE Performance"
+FIGURE_NAME="ALICE Work in Progress"
+#FIGURE_NAME="ALICE Prepliminary"
+TRIGGER_NAME="PYTHIA 8 + Geant 4"
 
 # Output the set options
 echo "Analysis of systematic"
@@ -91,5 +96,8 @@ echo "--trigger: $doTrigger"
 echo "--pass: $doPass"
 echo "--tagger: $doTagger"
 echo "--quarkhadron: $doQuarkHadronLevel"
+echo "--tracktuner: $doTrackTuner"
+echo "--track2coll: $doTrack2Coll"
+echo "--megeSV: $doMergeSV"
 
-root -x -l -b -q 'task/plotSys.cxx("'"$source"'", "'"$data"'", "'"$sim"'", "'"$TRIGGER_NAME"'", "'"$suffix"'","'"$doShower"'", "'"$doRadius"'", "'"$doTrackSelection"'", "'"$doTrigger"'", "'"$doPass"'", "'"$doTagger"'", "'"$doQuarkHadronLevel"'")'
+root -x -l -b -q 'task/plotSys.cxx("'"$ssource"'", "'"$data"'", "'"$sim"'", "'"$TRIGGER_NAME"'", "'"$FIGURE_NAME"'", "'"$suffix"'","'"$doShower"'", "'"$doRadius"'", "'"$doTrackSelection"'", "'"$doTrigger"'", "'"$doPass"'", "'"$doTagger"'", "'"$doQuarkHadronLevel"'", "'"$doTrackTuner"'", "'"$doTrack2Coll"'", "'"$doMergeSV"'")'
